77ad321f532662618ac1ad2efb15e5cbb99c3340
vicalloy <zbirder@gmail.com>
2010-05-26 00:08:13 +0800

    + insert attachment to editor. * event repeat binding

---
+++ sites/default/static/scripts/common.js
@@ -0,0 +1,8 @@
+function getExt(file){
+	return (-1 !== file.indexOf('.')) ? file.replace(/.*[.]/, '') : '';
+}
+function isImg(file) {
+	ext = getExt(file);
+	return ext && /^(jpg|png|jpeg|gif)$/.test(ext);
+	
+}
