1805dd7ddfbc0694e8d097a00c5654e6b3d8afea
vicalloy <zbirder@gmail.com>
2010-10-21 17:42:12 +0800

    - remove print

--- sites/default/manage.py
+++ sites/default/manage.py
@@ -5,7 +5,6 @@ import sys
 HERE = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(HERE, '../../src'))
 sys.path.insert(0, os.path.join(HERE, '../../requirements'))
-print HERE
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
