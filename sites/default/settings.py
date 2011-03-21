76dc9bdbffae5aa5452d8573289fbfca10e4e81a
vicalloy <zbirder@gmail.com>
2011-03-21 22:53:53 +0800

    * move media sites -> src/lbforum/

--- sites/default/settings.py
+++ sites/default/settings.py
@@ -37,7 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(HERE, 'static/')
+MEDIA_ROOT = os.path.join(HERE, 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
