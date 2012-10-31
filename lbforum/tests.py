5061bf52563537e714f8d014cac7c3c4629fb146
vicalloy <zbirder@gmail.com>
2012-10-31 15:27:09 +0800

    add some unit test

--- lbforum/tests.py
+++ lbforum/tests.py
@@ -12,7 +12,7 @@ class ViewsSimpleTest(ViewsBaseCase):
         resp = self.client.get(reverse('lbforum_index'))
         self.assertEqual(resp.status_code, 200)
 
-    def _test_recent(self):
+    def test_recent(self):
         resp = self.client.get(reverse('lbforum_recent'))
         self.assertEqual(resp.status_code, 200)
 
@@ -20,4 +20,16 @@ class ViewsSimpleTest(ViewsBaseCase):
         resp = self.client.get(reverse('lbforum_forum', args=("notexistforum", )))
         self.assertEqual(resp.status_code, 404)
         resp = self.client.get(reverse('lbforum_forum', args=("forum", )))
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.get(reverse('lbforum_forum', args=("forum", "topictype", )))
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.get(reverse('lbforum_forum', args=("forum", "topictype", "good")))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_topic(self):
+        resp = self.client.get(reverse('lbforum_topic', args=(1, )))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_lang_js(self):
+        resp = self.client.get(reverse('lbforum_lang_js'))
         self.assertEqual(resp.status_code, 200)
