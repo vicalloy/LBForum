ee5f02c235142f307f31601c88c5449457cbff94
vicalloy <zbirder@gmail.com>
2016-08-08 12:32:12 +0800

    add command init_lbforum_profile.py

--- lbforum/management/commands/init_lbforum_profile.py
+++ lbforum/management/commands/init_lbforum_profile.py
@@ -1,16 +1,15 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 
 from lbforum.models import LBForumUserProfile
-from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
     help = "Init LBForumUserProfile"
-    
+
     def handle(self, **options):
         users = User.objects.all()
         for o in users:
-            #LBForumUserProfile.objects.create(user=instance)
             try:
                 o.lbforum_profile
             except LBForumUserProfile.DoesNotExist:
