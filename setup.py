cd6330549f1331e4159e96290e5c0f698d401e6a
vicalloy <zbirder@gmail.com>
2016-08-19 22:15:21 +0800

    add Python3 to setup.classifiers

--- setup.py
+++ setup.py
@@ -49,6 +49,9 @@ setup(name='LBForum',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
           'Topic :: Utilities'
       ],
       )
